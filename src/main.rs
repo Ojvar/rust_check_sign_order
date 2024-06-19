b2b28fc92402a820006e9f608d820a86f4a8a5c0
Ojvar <MOjvar.AmirHossein@gmail.com>
2024-06-19 13:16:39 +0330

    init project

---
+++ src/main.rs
@@ -0,0 +1,92 @@
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+};
+
+struct Stack<T> {
+    elements: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Self {
+            elements: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, item: T) {
+        self.elements.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.elements.pop()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.elements.last()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.elements.is_empty()
+    }
+
+    fn size(&self) -> usize {
+        self.elements.len()
+    }
+}
+
+fn check_order(input_str: &str) -> bool {
+    let mut stack: Stack<char> = Stack::new();
+    let signs = HashMap::from([(')', '('), (']', '['), ('}', '{')]);
+
+    for c in input_str.chars() {
+        let counter_part_sign = signs.get(&c);
+        match counter_part_sign {
+            None => stack.push(c),
+            Some(t) => match stack.pop() {
+                None => return false,
+                Some(ts) => {
+                    if ts != *t {
+                        return false;
+                    }
+                }
+            },
+        }
+    }
+    if !stack.is_empty() {
+        return false;
+    }
+
+    true
+}
+
+fn main() {
+    let mut input_str = String::new();
+    io::stdout().flush().expect("Failed to flush stdout");
+    println!("Enter your input string: ");
+    io::stdin()
+        .read_line(&mut input_str)
+        .expect("Failed to read line");
+
+    let result = check_order(input_str.as_str());
+    println!("Result is {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correct_sequence() {
+        let input_str = "[](){}[{}][()][{()}]({[]})";
+        let result = check_order(input_str);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_incorrect_sequence() {
+        let input_str = "[}(){}[{}][()][{()}]({[]})";
+        let result = check_order(input_str);
+        assert_eq!(result, false);
+    }
+}
